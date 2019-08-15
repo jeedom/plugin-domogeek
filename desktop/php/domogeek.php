104680378fc951c754b15de33ee63972e8729df8
mso-29 <michel.souben@aliceadsl.fr>
2019-08-15 23:49:30 +0200

    Fix image link issue

--- desktop/php/domogeek.php
+++ desktop/php/domogeek.php
@@ -33,7 +33,7 @@ sendVarToJS('eqType', 'domogeek');
 	foreach ($eqLogics as $eqLogic) {
 		$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 		echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
-		echo '<img src="plugins/domogeek/doc/images/domogeek_icon.png" height="105" width="95" />';
+		echo '<img src="plugins/domogeek/docs/images/domogeek_icon.png" height="105" width="95" />';
 		echo "<br>";
 		echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogic->getHumanName(true, true) . '</span>';
 		echo '</div>';
